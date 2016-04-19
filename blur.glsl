bc66e02af52ef66fad5099f2270af1214ffaea47
James Anderson <jrsa@jrsa.co>
2016-04-19 06:13:21 -0700

    add saturation -> scal

--- blur.glsl
+++ blur.glsl
@@ -1,28 +1,42 @@
 precision mediump float;
 
-uniform sampler2D buffer;
+uniform sampler2D buffer, vid;
 uniform vec2 dims;
 uniform float width;
 uniform float amp;
 
 varying vec2 uv;
 
+vec3 rgb2hsv(vec3 c)
+{
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
 void main() {
 
-    mat2 sca = mat2(1.01, 0., 0., 0.99);
-    vec2 offs = vec2(1./dims.x, 1./dims.y);// * sca;
+    float s = rgb2hsv(texture2D(buffer, uv.st).rgb).s;
 
+    mat2 sca = mat2(1.+(0.01*s), 0., 0., 1.+(0.01*s));
+    vec2 offs = vec2(1./dims.x, 1./dims.y);
 
-    vec2 tc4 = uv.st;
-    vec2 tc1 = uv.st + vec2(0.0, -offs.t * width);
-    vec2 tc3 = uv.st + vec2(-offs.s * width, 0.0);
-    vec2 tc5 = uv.st + vec2(offs.s * width, 0.0);
-    vec2 tc7 = uv.st + vec2(0.0, offs.t * width);
+    vec2 src = uv.st * sca;
 
-    vec2 tc0 = uv.st + vec2(-offs.s * width, -offs.t * width);
-    vec2 tc2 = uv.st + vec2(offs.s * width, -offs.t * width);
-    vec2 tc6 = uv.st + vec2(-offs.s * width, offs.t * width);
-    vec2 tc8 = uv.st + vec2(offs.s * width, offs.t * width);
+    vec2 tc4 = src;
+    vec2 tc1 = src + vec2(0.0, -offs.t * width);
+    vec2 tc3 = src + vec2(-offs.s * width, 0.0);
+    vec2 tc5 = src + vec2(offs.s * width, 0.0);
+    vec2 tc7 = src + vec2(0.0, offs.t * width);
+
+    vec2 tc0 = src + vec2(-offs.s * width, -offs.t * width);
+    vec2 tc2 = src + vec2(offs.s * width, -offs.t * width);
+    vec2 tc6 = src + vec2(-offs.s * width, offs.t * width);
+    vec2 tc8 = src + vec2(offs.s * width, offs.t * width);
 
     vec4 col0 = texture2D(buffer, tc0);
     vec4 col1 = texture2D(buffer, tc1);
