98701324b3e5a0cad23ee4f805aed674cd6c59a9
James Anderson <jrsa@jrsa.co>
2016-04-19 03:56:04 -0700

    works with no real errors, except doesnt actaully do alpha channel or feedback cause video is written to fbo every time :p

--- index.js
+++ index.js
@@ -17,15 +17,18 @@ shell.on("gl-init", function() {
   var passthru_vs = glslify("./passthru.vs.glsl")
 
   updateShader = glShader(gl, passthru_vs, glslify("./2.fs.glsl"))
+  global.shader = updateShader
   drawShader = glShader(gl, passthru_vs, glslify("./draw.fs.glsl"))
  
+  var size = [shell.width, shell.height];
 
-  state = [ createFBO(gl, [864, 482]), createFBO(gl, [864, 482]) ]
+  state = [ createFBO(gl, size), createFBO(gl, size) ]
  
-  v_tex = createTexture(gl, [864, 482])
-
   vid = document.getElementById("poopy")
-  vid.play()
+  v_tex = createTexture(gl, [vid.videoWidth, vid.videoHeight])
+  vid.loop = true
+  //vid.play()
+  global.vid = vid
 
   drawShader.attributes.position.location = updateShader.attributes.position.location = 0
 })
@@ -38,8 +41,13 @@ shell.on("tick", function() {
   curState.bind()
 
   _gl = v_tex.gl
-  _gl.texImage2D(_gl.TEXTURE_2D, 0, _gl.RGBA, _gl.RGBA, gl.UNSIGNED_BYTE, vid)
-  state[0].color[0] = v_tex
+  if(vid.readyState === vid.HAVE_ENOUGH_DATA || 1) {
+    _gl.pixelStorei(_gl.UNPACK_FLIP_Y_WEBGL, true);
+    _gl.texImage2D(_gl.TEXTURE_2D, 0, _gl.RGBA, _gl.RGBA, gl.UNSIGNED_BYTE, vid);
+    _gl.texParameteri(_gl.TEXTURE_2D, _gl.TEXTURE_MAG_FILTER, _gl.NEAREST);
+    _gl.texParameteri(_gl.TEXTURE_2D, _gl.TEXTURE_MIN_FILTER, _gl.NEAREST);
+  }
+  prevState.color[0] = v_tex
  
   updateShader.bind()
   updateShader.uniforms.buffer = prevState.color[0].bind()
@@ -56,3 +64,4 @@ shell.on("gl-render", function(t) {
   drawShader.uniforms.buffer = state[current].color[0].bind()
   fillScreen(gl)
 })
+
